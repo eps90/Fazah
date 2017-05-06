5820c21a2e245063e49ca26061a719edac439881
Kuba Turek <ja@kubaturek.pl>
2017-05-06 11:39:36 +0200

    Fetch all messages in MessageRepository
    Add only several messages in fixture
    
    Implement basic find all method in MessageRepository

--- tests/Integration/Core/Repository/Impl/DoctrineMessageRepositoryTest.php
+++ tests/Integration/Core/Repository/Impl/DoctrineMessageRepositoryTest.php
@@ -3,8 +3,15 @@ declare(strict_types=1);
 
 namespace Eps\Fazah\Tests\Integration\Core\Repository\Impl;
 
+use Carbon\Carbon;
+use Eps\Fazah\Core\Model\Identity\CatalogueId;
+use Eps\Fazah\Core\Model\Identity\MessageId;
+use Eps\Fazah\Core\Model\Message;
+use Eps\Fazah\Core\Model\ValueObject\Metadata;
+use Eps\Fazah\Core\Model\ValueObject\Translation;
 use Eps\Fazah\Tests\Integration\Core\Repository\Impl\DataProvider\MessageRepositoryDataProvider;
 use Eps\Fazah\Tests\Resources\Fixtures\AddCatalogues;
+use Eps\Fazah\Tests\Resources\Fixtures\AddFewMessages;
 use Eps\Fazah\Tests\Resources\Fixtures\AddMessages;
 use Eps\Fazah\Tests\Resources\Fixtures\AddProjects;
 
@@ -24,5 +31,62 @@ class DoctrineMessageRepositoryTest extends DoctrineRepositoryTest
             AddCatalogues::class,
             AddMessages::class
         ];
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFindAllMessages(): void
+    {
+        $this->loadFixtures([
+            AddFewMessages::class
+        ]);
+        $expectedResult = [
+            Message::restoreFrom(
+                new MessageId('84decc43-283f-4089-8ded-f66513d1b54d'),
+                Translation::create(
+                    'my.message.3',
+                    'My message #3',
+                    'fr'
+                ),
+                new CatalogueId('94b1c887-f740-454a-b94e-706a0e5a0f41'),
+                Metadata::restoreFrom(
+                    Carbon::parse('2016-03-01 12:00:03'),
+                    Carbon::parse('2016-03-02 12:00:03'),
+                    true
+                )
+            ),
+            Message::restoreFrom(
+                new MessageId('fad9c222-02c6-4466-82f8-9345a84b52da'),
+                Translation::create(
+                    'my.message.2',
+                    'My message #2',
+                    'pl'
+                ),
+                new CatalogueId('94b1c887-f740-454a-b94e-706a0e5a0f41'),
+                Metadata::restoreFrom(
+                    Carbon::parse('2016-03-01 12:00:02'),
+                    Carbon::parse('2016-03-02 12:00:02'),
+                    true
+                )
+            ),
+            Message::restoreFrom(
+                new MessageId('af797da0-0959-4207-97f5-3dabf081a37f'),
+                Translation::create(
+                    'my.message.1',
+                    'My message #1',
+                    'en'
+                ),
+                new CatalogueId('94b1c887-f740-454a-b94e-706a0e5a0f41'),
+                Metadata::restoreFrom(
+                    Carbon::parse('2016-03-01 12:00:01'),
+                    Carbon::parse('2016-03-02 12:00:01'),
+                    true
+                )
+            )
+        ];
+        $actualResult = $this->getRepositoryInstance()->findAll();
+
+        static::assertEquals($expectedResult, $actualResult);
     }
 }
---
+++ tests/Resources/Fixtures/AddFewMessages.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace Eps\Fazah\Tests\Resources\Fixtures;
+
+use Carbon\Carbon;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Eps\Fazah\Core\Model\Catalogue;
+use Eps\Fazah\Core\Model\Identity\CatalogueId;
+use Eps\Fazah\Core\Model\Identity\MessageId;
+use Eps\Fazah\Core\Model\Identity\ProjectId;
+use Eps\Fazah\Core\Model\Message;
+use Eps\Fazah\Core\Model\ValueObject\Metadata;
+use Eps\Fazah\Core\Model\ValueObject\Translation;
+
+final class AddFewMessages extends AbstractFixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $catalogueId = new CatalogueId('94b1c887-f740-454a-b94e-706a0e5a0f41');
+        $catalogue = Catalogue::restoreFrom(
+            $catalogueId,
+            'Lonely catalogue',
+            new ProjectId('ff054881-6ddb-4f78-bdfb-681068b610ee'),
+            Metadata::restoreFrom(
+                Carbon::parse('2016-03-01 12:00:00'),
+                Carbon::parse('2016-03-02 12:00:00'),
+                true
+            )
+        );
+
+        $manager->persist($catalogue);
+
+        $messages = [
+            Message::restoreFrom(
+                new MessageId('af797da0-0959-4207-97f5-3dabf081a37f'),
+                Translation::create(
+                    'my.message.1',
+                    'My message #1',
+                    'en'
+                ),
+                $catalogueId,
+                Metadata::restoreFrom(
+                    Carbon::parse('2016-03-01 12:00:01'),
+                    Carbon::parse('2016-03-02 12:00:01'),
+                    true
+                )
+            ),
+            Message::restoreFrom(
+                new MessageId('fad9c222-02c6-4466-82f8-9345a84b52da'),
+                Translation::create(
+                    'my.message.2',
+                    'My message #2',
+                    'pl'
+                ),
+                $catalogueId,
+                Metadata::restoreFrom(
+                    Carbon::parse('2016-03-01 12:00:02'),
+                    Carbon::parse('2016-03-02 12:00:02'),
+                    true
+                )
+            ),
+            Message::restoreFrom(
+                new MessageId('84decc43-283f-4089-8ded-f66513d1b54d'),
+                Translation::create(
+                    'my.message.3',
+                    'My message #3',
+                    'fr'
+                ),
+                $catalogueId,
+                Metadata::restoreFrom(
+                    Carbon::parse('2016-03-01 12:00:03'),
+                    Carbon::parse('2016-03-02 12:00:03'),
+                    true
+                )
+            )
+        ];
+
+        foreach ($messages as $message) {
+            $manager->persist($message);
+        }
+
+        $manager->flush();
+    }
+}
