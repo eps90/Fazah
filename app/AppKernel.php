16acf1918c3d92d3ae4b3a95bc68dedf8951ce6c
Kuba Turek <ja@kubaturek.pl>
2017-05-07 12:24:32 +0200

    Install Tactician and Tactician Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new \League\Tactician\Bundle\TacticianBundle(),
             new \Eps\Fazah\FazahBundle\FazahBundle(),
         ];
 
