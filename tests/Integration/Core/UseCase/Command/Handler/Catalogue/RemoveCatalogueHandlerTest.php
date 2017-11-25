ba8865719dea77da3ba9ba0208f8eb38e6c7b530
Kuba Turek <ja@kubaturek.pl>
2017-11-25 19:06:10 +0100

    [#2] Create a handler for RemoveCatalogue command

---
+++ tests/Integration/Core/UseCase/Command/Handler/Catalogue/RemoveCatalogueHandlerTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Eps\Fazah\Tests\Integration\Core\UseCase\Command\Handler\Catalogue;
+
+use Eps\Fazah\Core\Model\Identity\CatalogueId;
+use Eps\Fazah\Core\Repository\CatalogueRepository;
+use Eps\Fazah\Core\Repository\Exception\CatalogueRepositoryException;
+use Eps\Fazah\Core\UseCase\Command\Catalogue\RemoveCatalogue;
+use Eps\Fazah\Tests\Resources\Fixtures\AddFewCatalogues;
+use League\Tactician\CommandBus;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class RemoveCatalogueHandlerTest extends WebTestCase
+{
+    /**
+     * @var CommandBus
+     */
+    private $commandBus;
+    
+    /**
+     * @var CatalogueRepository
+     */
+    private $catalogueRepo;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $container = $this->getContainer();
+        $this->commandBus = $container->get('tactician.commandbus');
+        $this->catalogueRepo = $container->get('fazah.repository.catalogue');
+        
+        $this->loadFixtures([
+            AddFewCatalogues::class
+        ]);
+    }
+    
+    /**
+     * @test
+     */
+    public function itShouldRemoveCatalogueWithGivenId(): void
+    {
+        $catalogueId = new CatalogueId('12853ef6-43a5-4e7f-8ff5-3fb47ef10a07');
+        $command = new RemoveCatalogue($catalogueId);
+        
+        $this->commandBus->handle($command);
+        
+        $this->expectException(CatalogueRepositoryException::class);
+        $this->catalogueRepo->find($catalogueId);
+    }
+}
---
+++ tests/Unit/Core/UseCase/Command/Handler/Catalogue/RemoveCatalogueHandlerTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Eps\Fazah\Tests\Unit\Core\UseCase\Command\Handler\Catalogue;
+
+use Eps\Fazah\Core\Model\Identity\CatalogueId;
+use Eps\Fazah\Core\Repository\CatalogueRepository;
+use Eps\Fazah\Core\UseCase\Command\Catalogue\RemoveCatalogue;
+use Eps\Fazah\Core\UseCase\Command\Handler\Catalogue\RemoveCatalogueHandler;
+use PHPUnit\Framework\TestCase;
+
+class RemoveCatalogueHandlerTest extends TestCase
+{
+    /**
+     * @var CatalogueRepository|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $catalogueRepo;
+    
+    /**
+     * @var RemoveCatalogueHandler
+     */
+    private $handler;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->catalogueRepo = $this->createMock(CatalogueRepository::class);
+        $this->handler = new RemoveCatalogueHandler($this->catalogueRepo);
+    }
+    
+    /**
+     * @test
+     */
+    public function itShouldCallRepositoryToRemoveACatalogue(): void
+    {
+        $catalogueId = new CatalogueId('12853ef6-43a5-4e7f-8ff5-3fb47ef10a07');
+        $command = new RemoveCatalogue($catalogueId);
+        
+        $this->catalogueRepo->expects(static::once())
+            ->method('remove')
+            ->with($catalogueId);
+        
+        $this->handler->handle($command);
+    }
+}
